a1e72bd66f9885e3b681365c5104a278f7cfa1b5
Michaelwmx <Michaelwmx@users.noreply.github.com>
2019-12-11 23:17:31 +0800

    二维偏+ LIS

--- XDU1469.cpp
+++ XDU1469.cpp
@@ -6,14 +6,13 @@ using namespace std;
 
 struct point{
     int x,y;
-    point():x(0),y(0){}
 }p[MAXN];
 
 bool cmp(point a,point b){
-    if(a.y-a.x!=b.y-b.x)
-        return a.y-a.x<b.y-b.x;
+    if(a.y+a.x!=b.y+b.x)
+        return a.y+a.x<b.y+b.x;
     else
-        return a.x+a.y<b.x+b.y;
+        return a.y-a.x<b.y-b.x;
 }
 
 int n,arr[MAXN],len;
@@ -26,9 +25,9 @@ int main(){
     sort(p+1,p+1+n,cmp);
     len=0;
     arr[0]=0;
-    for(int i=lower_bound(p+1,p+1+len,point(),cmp)-p;i<=n;i++){
-        int t=p[i].y+p[i].x;
-        if(t<0) continue;
+    for(int i=1;i<=n;i++){
+        int t=p[i].y-p[i].x;
+        if(t<0 || p[i].x+p[i].y<0) continue;
         if(arr[len]<=t)
             arr[++len]=t;
         else{
