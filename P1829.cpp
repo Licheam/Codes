993f6c489d0d010c3bd9e60136eba8d05f862d6f
Michaelwmx <Michaelwmx@users.noreply.github.com>
2020-01-30 19:10:52 +0800

    一道莫比乌斯反演题。 在brute的基础上再换元，预处理积性函数卷积O(n)，一个分块O(√n)

--- P1829.cpp
+++ P1829.cpp
@@ -11,51 +11,44 @@ int n,m,lim;
 int p[MAXN],cnt;
 bool inp[MAXN];
 
-LL mu[MAXN],smu[MAXN];
+LL vid[MAXN],svid[MAXN];//vid=(n^2*mu(n))*id
 
 void pre(){
-	memset(inp+1,0,lim*sizeof(inp[0]));
-	inp[0]=inp[1]=1;
-	cnt=0;
-	smu[0]=0;
-	smu[1]=mu[1]=1;
-	for(int i=2;i<=lim;i++){
-		if(!inp[i]) p[++cnt]=i, mu[i]=-1;
-		for(int j=1;j<=cnt && i*p[j]<=lim;j++){
-			inp[i*p[j]]=1;
-			if(i%p[j]){
-				mu[i*p[j]]=-mu[i];
-			}
-			else{
-				mu[i*p[j]]=0;
-				break;
-			}
-		}
-		smu[i]=(smu[i-1]+1LL*i*i%MOD*mu[i]%MOD+MOD)%MOD;
-	}
+    memset(inp+1,0,lim*sizeof(inp[0]));
+    inp[0]=inp[1]=1;
+    cnt=0;
+    svid[0]=0;
+    svid[1]=vid[1]=1;
+    for(LL i=2;i<=lim;i++){
+        if(!inp[i]) p[++cnt]=i, vid[i]=((1-i)*i%MOD+MOD)%MOD;
+        for(LL j=1;j<=cnt && i*p[j]<=lim;j++){
+            inp[i*p[j]]=1;
+            if(i%p[j]){
+                vid[i*p[j]]=vid[p[j]]*vid[i]%MOD;
+            }
+            else{
+                vid[i*p[j]]=vid[i]*p[j]%MOD;
+                break;
+            }
+        }
+        svid[i]=(svid[i-1]+vid[i])%MOD;
+    }
 }
 
-LL sumf(LL d){
-	LL res=0;
-	LL bound=lim/d, nd=n/d, md=m/d;
-	for(LL i=1;i<=bound;i++){
-		LL j=min(nd/(nd/i),md/(md/i));
-		res=(res+(smu[j]-smu[i-1]+MOD)*((1+nd/i)*(nd/i)/2%MOD)%MOD*((1+md/i)*(md/i)/2%MOD)%MOD)%MOD;
-		i=j;
-	}
-	return res;
+LL sum(LL x){
+    return x*(x+1)/2%MOD;
 }
 
 int main(){
-	scanf("%d %d", &n, &m);
-	lim=min(n,m);
-	pre();
-	LL ans=0;
-	for(LL i=1;i<=lim;i++){
-		LL j=min(n/(n/i),m/(m/i));
-		ans=(ans+(((i+j)*(j-i+1)/2)%MOD*sumf(i)%MOD)+MOD)%MOD;
-		i=j;
-	}
-	printf("%lld\n", ans);
-	return 0;
+    scanf("%d %d", &n, &m);
+    lim=min(n,m);
+    pre();
+    LL ans=0;
+    for(LL i=1;i<=lim;i++){
+        LL j=min(n/(n/i),m/(m/i));
+        ans=(ans+(svid[j]-svid[i-1]+MOD)*((sum(n/i)*sum(m/i))%MOD)%MOD)%MOD;
+        i=j;
+    }
+    printf("%lld\n", ans);
+    return 0;
 }
