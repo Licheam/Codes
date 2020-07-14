9d4eb697a615fbcb0dbbca1953d50ebced0f12aa
Michaelwmx <wmx_wmx@foxmail.com>
2020-07-14 16:54:53 +0800

    给定两个序列A（150000）,B（40000），求B在A中有多少个匹配，使得对应位置A[i]>=B[j]。分别对A，B排序，从大到小，对每个B看有多少个位置的A满足A>B，然后bitset储存，ans&=（mark>>index）记录所有可能的初始节点取并集。

---
+++ [NowC]5667G.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define MAXN 150005
+#define LL long long
+using namespace std;
+
+bitset<MAXN> ans,mark;
+pair<int,int> a[MAXN],b[MAXN];
+void solve() {
+    int n,m;
+    scanf("%d %d", &n, &m);
+    for(int i=1;i<=n;i++){
+        scanf("%d", &a[i].first);
+        a[i].second=i;
+    }
+        scanf("%d", &b[i].first);
+        b[i].second=i;
+    }
+    sort(a+1,a+1+n);
+    sort(b+1,b+1+m);
+    ans.set();
+    mark.reset();
+    for(int i=m,j=n;i>=1;i--) {
+        for(;j && a[j].first>=b[i].first;j--)
+            mark.set(a[j].second);
+        ans&=(mark>>b[i].second);
+    }
+    printf("%d\n", ans.count());
+}
+
+int main() {
+    int T=1,cas=1;
+    // scanf("%d", &T);
+    while(T--) {
+        // printf("Case #%d: ", cas++);
+        solve();
+    }
+    return 0;
+}
